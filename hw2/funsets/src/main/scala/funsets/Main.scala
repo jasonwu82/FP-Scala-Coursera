1fd6ba0330e6edb7d3f6a01f60c39922afe5fc53
jasonwu82 <jasonwu082@gmail.com>
2017-05-17 15:55:36 +0800

    complete and pass hw2

--- hw2/funsets/src/main/scala/funsets/Main.scala
+++ hw2/funsets/src/main/scala/funsets/Main.scala
@@ -5,4 +5,11 @@ object Main extends App {
 
   println(contains(singletonSet(1), 1))
   println(contains(singletonSet(1), 2))
+
+  var s = singletonSet(-10)
+  for(i <- -9 to 10){
+    s = union(s,singletonSet(i))
+  }
+  def f1(x:Int) = x*3
+  printSet(map(s,f1))
 }
